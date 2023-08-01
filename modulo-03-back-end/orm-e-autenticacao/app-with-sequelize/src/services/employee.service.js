f7453b5315942132e62bd2be19bb8701dbc603df
Pedro Medeiros <pedropwks@gmail.com>
2023-07-31 21:58:49 -0300

    Adiciona anotações de transações ORM, relacionamento N:N com projeto teste

--- modulo-03-back-end/orm-e-autenticacao/app-with-sequelize/src/services/employee.service.js
+++ modulo-03-back-end/orm-e-autenticacao/app-with-sequelize/src/services/employee.service.js
@@ -1,6 +1,12 @@
 // src/services/employee.service.js
 
 const { Address, Employee } = require('../models/');
+const Sequelize = require('sequelize');
+const config = require('../config/config');
+
+const env = process.env.NODE_ENV || 'development';
+// Ajustamos para usar a configuração correta para nosso ambiente
+const sequelize = new Sequelize(config[env]);
 
 const getAll = async () => {
   const users = await Employee.findAll({
@@ -27,7 +33,26 @@ const getById = async (id) => {
 //   return employee;
 // }
 
-module.exports = { 
+const insert = async ({ firstName, lastName, age, city, street, number }) => {
+  const result = await sequelize.transaction(async (t) => {
+    const employee = await Employee.create({
+      firstName, lastName, age,
+    }, { transaction: t });
+
+    await Address.create({
+      city, street, number, employeeId: employee.id
+    }, { transaction: t });
+    return employee;
+  });
+
+  return result;
+  // Se chegou até aqui é porque as operações foram concluídas com sucesso,
+  // não sendo necessário finalizar a transação manualmente.
+  // `result` terá o resultado da transação, no caso um empregado e o endereço cadastrado
+};
+
+module.exports = {
   getAll,
   getById,
+  insert,
 };
